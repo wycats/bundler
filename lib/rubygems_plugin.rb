e42b04da3585e01d3f28937d59e17c71309dee15
Carl Lerche <carllerche@mac.com>
2010-02-03 15:02:19 -0800

    Removing all the code

--- lib/rubygems_plugin.rb
+++
@@ -1,6 +0,0 @@
-require 'rubygems/command_manager'
-require 'bundler/commands/bundle_command'
-require 'bundler/commands/exec_command'
-
-Gem::CommandManager.instance.register_command :bundle
-Gem::CommandManager.instance.register_command :exec
